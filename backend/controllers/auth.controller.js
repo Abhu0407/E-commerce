6c80d166db69e6de5bac2e5d0f3c4a6cecbc31d5
Abhishek Rathore <144262140+Abhu0407@users.noreply.github.com>
2025-07-24 23:23:36 +0530

    Add files via upload

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+    res.send("gdfycgas");
+}
+
+export const login = async (req, res) => {
+    res.send("gdfycgas");
+}
+
+export const logout = async (req, res) => {
+    res.send("gdfycgas");
+}
+
